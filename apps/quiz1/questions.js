78c453b6a0ccc132fe5ffbb0fbd58dbb7bf4542b
Nasir Nooruddin <nasir@nasir.id>
2024-01-10 22:29:47 +0700

    update

---
+++ apps/quiz1/questions.js
@@ -0,0 +1,51 @@
+Let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        options: [
+            
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        options: [
+            
+        ]
+    }
+];
